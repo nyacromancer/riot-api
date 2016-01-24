2cfe00036440f068bde02a1e1989970103be05e6
IEACATADB <nyacromancer@gmail.com>
2016-01-24 17:24:07 +0300

    bunch of folders and stuff

---
+++ spec/gemname_spec.rb
@@ -0,0 +1,10 @@
+describe GemName do 
+  before :all do
+    @ra = GemName.new 
+  end
+  describe '#new' do 
+    it "should raise and error if initialized without API key" do
+      
+    end
+  end
+end
